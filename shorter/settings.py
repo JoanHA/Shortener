029b3778611a0900e1c0ee0dd41b166bfc09b4a2
SebastianHurtado19 <joan.hurtado@infodeclat.com>
2024-08-03 15:06:10 -0500

    LAst commit

--- shorter/settings.py
+++ shorter/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'shorter.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'shorterdb',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.mysql',
+    #         'NAME': 'shorterdb',
+    #         'USER': 'root',
+    #         'PASSWORD': '',
+    #         'HOST': 'localhost',
+    #         'PORT': '3306',
+    #     }
 }
 
 
